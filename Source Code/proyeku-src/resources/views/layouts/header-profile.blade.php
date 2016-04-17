a77b3883ed17300ea9c13f97582b6148e01807d3
Rifki Nurfaiz <rifkinurfaiz@gmail.com>
2016-04-17 12:20:21 +0700

    Minor update layout

--- Source Code/proyeku-src/resources/views/layouts/header-profile.blade.php
+++ Source Code/proyeku-src/resources/views/layouts/header-profile.blade.php
@@ -64,7 +64,7 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
     {{-- <script src="{{ elixir('js/app.js') }}"></script> --}}
 
-    @extends('layouts.master-footer')
+    @extends('layouts.footer')
     @section('footer')
     @parent
     @stop
