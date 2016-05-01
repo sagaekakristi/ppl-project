42cc1d24acfe96e0f813d48847cc0924f43a9503
kemasm <maxx23295@gmail.com>
2016-05-01 16:34:51 +0700

    Remove unnecessary lines

--- Source Code/proyeku-src/resources/views/search.blade.php
+++ Source Code/proyeku-src/resources/views/search.blade.php
@@ -8,54 +8,7 @@
 
 @section('content')
 <div class="container" id="body">
-    <!--
-    <div class="row">
-        <div class="col-md-10 col-md-offset-1">
-            <div class="panel panel-default">
-                <div class="panel-heading">Search Result of {{$search}}</div>
-                
-                <div class="panel-body">
-                    <form class="col-md-10 col-md-offset-1" role="search" style="margin-bottom: 170px;" action="{{url('/searchredirect')}}">
-            			<div class="input-group">
-                			<input type="text" class="form-control" placeholder="Search" style="height: 40px;" name="search">
-                			<div class="input-group-btn">
-                    			<button class="btn btn-default" type="submit" style="background-color: #F26151; color: white; height: 40px;"><strong>Cari Freelancer</strong></button>
-                			</div>
-            			</div>
-       				</form>
-
-                    @if(isset($message))
-                    <div class="bg-warning" style='padding: 20px; margin-top: 50px;'>{{$message}}</div>
-                    
-                    @else
-                    <div class="bg-warning" style='padding: 20px'>{{$search}}</div>
-
-                    <div>
-                        @foreach ($jobs as $job)
-                            <a href="{{url('/job/'.$job->id)}}">{{ $job->name }}</a>
-                            <br/>
-                            {{ $job->judul }}
-                            <br/>
-                            {{ $job->alamat }}
-                            <br/>
-                            {{ $job->deskripsi }}
-                            <br/>
-                            {{ $job->upah_max }}
-                            {{ $job->upah_min }}
-                            <br/>
-                            <br/>
-                        @endforeach
-                        {!! $jobs->links() !!}
-                    </div>
-
-                    @endif
-
-
-                </div>
-            </div>
-        </div>
-    </div>
-    -->
+    
     <hr class="hr"></hr>
     @if(isset($message))
     <div class="bg-warning" style='padding: 20px'>{{$message}}</div>
