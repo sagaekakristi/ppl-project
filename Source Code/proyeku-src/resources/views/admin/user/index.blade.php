cb05de51fcfe58741f84398704856929acf02751
ariejoseph <ariejlo19@gmail.com>
2016-05-11 17:30:04 +0700

    Fix some conflict after merging admin to master

--- Source Code/proyeku-src/resources/views/admin/user/index.blade.php
+++ Source Code/proyeku-src/resources/views/admin/user/index.blade.php
@@ -11,32 +11,6 @@
 	<div class="col-md-10 col-md-offset-1">
 		<h1 style="font-family: Titillium Web;">List Of User</h1>
 		<br>
-		<table class="table table-condensed">
-			<tr>
-				<th class="admin-head">Name</th>
-				<th class="admin-head">Email</th>
-				<th class="admin-head"></th>
-				<th class="admin-head"></th>
-			</tr>
-			@foreach ($users as $user)
-			<tr>
-				<td>
-					<a href="/admin/manage/user/{{ $user->id }}">{{ $user->name }}</a>
-				</td>
-				<td>
-					<a href="/admin/manage/user/{{ $user->id }}">{{ $user->email }}</a>
-				</td>
-				<td class="icon">
-					<a href="/admin/manage/user/{{ $user->id }}/edit" class="btn btn-info pull-left" style="margin-right: 3px;">Edit</a>
-				</td>
-				<td class="icon">
-					{{ Form::open(['url' => '/admin/manage/user/' . $user->id, 'method' => 'DELETE']) }}
-					{{ Form::submit('Delete', array('class' => 'btn btn-danger ','onclick'=>'return confirm("Are you sure want to delete this user?")')) }}
-					{{ Form::close() }}
-				</td>
-			</tr>
-			@endforeach
-		</table>
 		<div>
 			<div>
 				<a href="/admin/manage/user/create" class="btn btn-success">Add user</a>
--- Source Code/proyeku-src/resources/views/admin/user/show.blade.php
+++ Source Code/proyeku-src/resources/views/admin/user/show.blade.php
@@ -12,54 +12,9 @@
 	@section('content')
 	<div class="container" id="body">
 		<div class="col-md-8 col-md-offset-2" style="background-color: white; border-radius: 10px; margin-top: 30px; margin-bottom: 30px;">
-			<h1>{{ $user->name }}</h1>
-			<div>
-				<table class="table" style="font-size: 18px;">
-					<tr>
-						<td>
-							Name
-						</td>
-						<td>
-							: {{$user->name}}
-						</td>
-					</tr>
-					<tr>
-						<td>
-							Email
-						</td>
-						<td>
-							: {{$user->email}}
-						</td>
-					</tr>
-					<tr>
-						<td>
-							Tgl lahir
-						</td>
-						<td>
-							: {{$userinfo->tanggal_lahir}}
-						</td>
-					</tr>
-					<tr>
-						<td>
-							Alamat
-						</td>
-						<td>
-							: {{$userinfo->alamat}}
-						</td>
-					</tr>
-					<tr>
-						<td>
-							Jenis kelamin
-						</td>
-						<td>	
-							: {{$userinfo->jenis_kelamin}}
-						</td>
-					</tr>
-				</table>
-			</div>
 		<h1>{{ $user->name }}</h1>
 		<div>
-			<table class="table">
+			<table class="table" style="font-size: 18px;">
 				<tr>
 					<td>
 						User ID
