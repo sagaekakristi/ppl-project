26864ecc4c2872630a5cffe59e0bba715f1927f2
Mgs. M. Rizqi Fadhlurrahman <rizqirizqi23@gmail.com>
2016-05-11 12:52:30 +0700

    Integrasi login dan register menggunakan Facebook
    + Update route + menghapus route ke /home
    + Membuat table SocialAccounts
    + Membuat model SocialAccounts dan service nya
    + Update controller SocialAccounts
    + Menambahkan development notes

--- Source Code/proyeku-src/app/Http/routes.php
+++ Source Code/proyeku-src/app/Http/routes.php
@@ -36,8 +36,6 @@ Route::group(['middleware' => ['web']], function () {
 
 Route::group(['middleware' => 'web'], function () {
     Route::auth();
-
-    Route::get('/home', 'HomeController@index');
 });
 
 Route::get('/searchredirect', function(){
---
+++ Source Code/proyeku-src/app/SocialAccountService.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App;
+
+use Laravel\Socialite\Contracts\User as ProviderUser;
+
+class SocialAccountService
+{
+    public function createOrGetUser(ProviderUser $providerUser)
+    {
+// Cari akun di db
+        $account = SocialAccount::whereProvider('facebook')
+        ->whereProviderUserId($providerUser->getId())
+        ->first();
+
+        if ($account) {
+// Kalo ada > return user
+            return $account->user;
+        } else {
+// Kalo gaada > register
+            $account = new SocialAccount([
+                'provider_user_id' => $providerUser->getId(),
+                'provider' => 'facebook'
+                ]);
+
+            $user = User::whereEmail($providerUser->getEmail())->first();
+
+            if (!$user) {
+
+                $user = User::create([
+                    'email' => $providerUser->getEmail(),
+                    'name' => $providerUser->getName(),
+                    ]);
+            }
+
+            $account->user()->associate($user);
+            $account->save();
+
+            return $user;
+
+        }
+
+    }
+}
