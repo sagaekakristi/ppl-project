d98ddcf547cecab69ddec9c2e7823ede8b916cd7
ariejoseph <ariejlo19@gmail.com>
2016-05-15 17:43:44 +0700

    Revert "Merge branch 'master' of https://github.com/sagaekakristi/ppla02"
    This reverts commit 2140c19fa3c597982bba1e0b0df242513324aa7e.

--- Source Code/proyeku-src/app/Http/Controllers/ProfilePageController.php
+++ Source Code/proyeku-src/app/Http/Controllers/ProfilePageController.php
@@ -34,112 +34,15 @@ class ProfilePageController extends Controller
         $jenis_kelamin = $user_info->jenis_kelamin;
         $nama = $users->name;
 
+        // get all the job
         $user_info = UserInfo::find($logged_user_id);
         $freelancer_info = FreelancerInfo::find($user_info->user_id);
         $jobs = Job::where('freelancer_info_id', $freelancer_info->user_info_id)->get();
 
+        // load the view and pass the jobs
         return View::make('profile')
         ->with('jobs', $jobs)
         ->with('user_info', $user_info)
         ->with('users', $users);
     }
-
-    public function editGeneral()
-    {
-        $logged_user_id = Auth::user()->id;
-        $user_info = UserInfo::find($logged_user_id);
-        $user = user::find($user_info->user_id);
-
-        return View::make('general')
-        ->with('user_info', $user_info)
-        ->with('user', $user);
-    }
-
-    public function editInfo()
-    {
-        $logged_user_id = Auth::user()->id;
-        $user_info = UserInfo::find($logged_user_id);
-        $user = user::find($user_info->user_id);
-
-        return View::make('info')
-        ->with('user_info', $user_info)
-        ->with('user', $user);
-    }
-
-   /* public function updateGeneral() 
-    {
-
-        $rules = array(
-            'image'         => 'required|mimes:png',
-            'email'         => 'required',
-            'password'      => 'required',
-            );
-
-        $validator = Validator::make(Input::all(), $rules);
-
-        if ($validator->fails()) {
-            return Redirect::to('/profile/edit/general');
-        } else {
-            $id = Auth::user()->id;
-
-            $updated_user = User::find($id);
-            $updated_user->email = Input::get('email');
-            $updated_user->password = Input::get('password');
-
-            $updated_user->save();
-
-            Session::flash('message', 'Successfully updated job!');
-            return Redirect::to('/profile');
-        }
-    }*/
-
-
-    public function updateInfo()
-    {
-
-        $rules = array(
-            'alamat'        => 'required',
-            'jenis_kelamin' => 'required',
-            'tanggal_lahir' => 'required'
-            );
-        $validator = Validator::make(Input::all(), $rules);
-
-        if ($validator->fails()) {
-            return Redirect::to('/profile/edit/info');
-        } else {
-            $id = Auth::user()->id;
-
-            $updated_user_info = UserInfo::find($id);
-            $updated_user_info->tanggal_lahir = Input::get('tanggal_lahir');
-            $updated_user_info->alamat = Input::get('alamat');
-            $updated_user_info->jenis_kelamin = Input::get('jenis_kelamin');
-
-            $updated_user_info->save();
-
-            Session::flash('message', 'Successfully updated job!');
-            return Redirect::to('/profile');
-        }
-    }
-
-    //Store photo
-    /*public function store(ProductRequest $request)
-    {
-
-        $product = new Product(array(
-            'name' => $request->get('name'),
-            'sku'  => $request->get('sku')
-            ));
-
-        $product->save();
-
-        $imageName = $product->id . '.' . 
-        $request->file('image')->getClientOriginalExtension();
-
-        $request->file('image')->move(
-            base_path() . '/public/images/catalog/', $imageName
-            );
-
-        return \Redirect::route('admin.products.edit', 
-            array($product->id))->with('message', 'Product added!');    
-        }*/
-    }
+}
