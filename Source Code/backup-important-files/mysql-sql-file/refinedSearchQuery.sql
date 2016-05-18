f0791a8778853bde6e9195d8f7a8152caae128c2
kemasm <maxx23295@gmail.com>
2016-05-18 16:57:10 +0700

    bug fix

--- Source Code/backup-important-files/mysql-sql-file/refinedSearchQuery.sql
+++ Source Code/backup-important-files/mysql-sql-file/refinedSearchQuery.sql
@@ -2,4 +2,11 @@ select u.name, v.alamat, j.judul, j.deskripsi, j.upah_max, j.upah_min
 from job j
 join users u on j.freelancer_info_id = u.id
 join user_info v on u.id = v.user_id
-order by j.id 
+join job_category jc on j.id = jc.job_id
+join category c on c.id = jc.category_id
+where j.judul like '%%'
+and v.alamat like '%%'
+and j.upah_max <= 999999999
+and j.upah_min >= 0
+and c.kategori like '%%'
+order by u.
--- Source Code/proyeku-src/app/Http/Controllers/SearchController.php
+++ Source Code/proyeku-src/app/Http/Controllers/SearchController.php
@@ -31,7 +31,7 @@ class SearchController extends Controller{
 
         $order = $request->order;
         if(is_null($order) || $order===""){
-            $upah_max = "users.user_rating";
+            $order = "user_info.user_rating";
         }
         //dump($order);
 
@@ -51,7 +51,7 @@ class SearchController extends Controller{
                     -> where('job.upah_max', '<=', $upah_max)
                     -> where('job.upah_min', '>=', $upah_min)
                     -> where('category.kategori', 'LIKE', '%'.$kategori.'%')
-                    -> orderBy($order);
+                    -> orderBy($order)
                     -> paginate(2);
                     //->get();
         //dd($jobs);
